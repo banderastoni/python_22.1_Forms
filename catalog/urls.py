83272da9b63a24866de57d4e631a87d7b43c3d2d
banderastoni <banderastoni@rambler.ru>
2023-12-20 23:15:49 +0300

    20.2 домашка готова

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import index, contact
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('contact/', contact, name='contact')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def index(request):
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/index.html', context)
+
+
+def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
         print(f'{name} ({email}): {message}')
 
-    return render(request, 'catalog/index.html')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
